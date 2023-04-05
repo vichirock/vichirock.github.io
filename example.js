68db52decdf26f1cce0b9c5d1bc97822570c4b26
vichirock <113020769+vichirock@users.noreply.github.com>
2023-04-05 11:39:26 +0200

    lidt javascript

--- example.js
+++ example.js
@@ -5,16 +5,12 @@ let answer3 = 'He is called "Johnny"';
 document.getElementById("demo").innerHTML =
 answer1 + "<br>" + 
 answer2 + "<br>" + 
-answer3;
+answer3; 
 
 
 
-let x1 = 34.00;
-let x2 = 34;
-let x3 = 3.14;
 
-document.getElementById("numbers").innerHTML =
-x1 + "<br>" + x2 + "<br>" + x3;
+
 
 
 
