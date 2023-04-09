03c22bef9df5a5074fb5af327d1d895024a0f240
vichirock <113020769+vichirock@users.noreply.github.com>
2023-04-09 11:32:43 +0200

    lidt variables i javascript

---
+++ variables.js
@@ -0,0 +1,17 @@
+var x = 5;
+var y = 6;
+var z = x + y;
+document.getElementById("demo").innerHTML =
+"The value of z is: " + z;
+
+// Create an Array:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Change an element:
+cars[0] = "Toyota";
+
+// Add an element:
+cars.push("Audi");
+
+// Display the Array:
+document.getElementById("demo1").innerHTML = cars; 
