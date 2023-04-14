a0f79c03eda7628307e24e4dfc76cac948640cdc
vichirock <113020769+vichirock@users.noreply.github.com>
2023-04-14 09:42:56 +0200

    lidt responsive

---
+++ responsive-full-background-image-master/example2.js
@@ -0,0 +1,6 @@
+let x = 5;
+let y = 5;
+let z = 6;
+
+document.getElementById("demo").innerHTML =
+(x == y) + "<br>" + (x == z);
---
+++ responsive-full-background-image-master/functionex.js
@@ -0,0 +1,5 @@
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+document.getElementById("demo").innerHTML = myFunction(4, 3);
