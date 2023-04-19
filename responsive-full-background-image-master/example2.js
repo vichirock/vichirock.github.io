141917a9987108c82236478258d36765010af320
vichirock <113020769+vichirock@users.noreply.github.com>
2023-04-19 10:14:39 +0200

    lidt all

--- responsive-full-background-image-master/example2.js
+++
@@ -1,6 +0,0 @@
-let x = 5;
-let y = 5;
-let z = 6;
-
-document.getElementById("demo").innerHTML =
-(x == y) + "<br>" + (x == z);
--- responsive-full-background-image-master/functionex.js
+++
@@ -1,5 +0,0 @@
-
-function myFunction(p1, p2) {
-  return p1 * p2;
-}
-document.getElementById("demo").innerHTML = myFunction(4, 3);
